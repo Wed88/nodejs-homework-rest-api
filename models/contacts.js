2e236f54335c87e3347f6a06a9dd700b2afbf72d
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2022-05-28 01:20:53 +0300

    выполняет первый шаг и post запрос на всю коллекцию

--- models/contacts.js
+++ models/contacts.js
@@ -1,12 +1,49 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require("path");
+const uuid = require("uuid")
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find(item => item.id === contactId);
+  if(!contact){
+    return null;
+  }
+  return contact;
+}
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(contact => contact.id === contactId);
+
+  const removeContact = contacts[index];
+  if(index !== -1) {
+    contacts.splice(index,1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  }
+  return removeContact;
+}
+
+const addContact = async (name, email, phone) => {
+  const newContact = {
+    id: uuid.v4(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+}
 
 const updateContact = async (contactId, body) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,8 +2,28 @@ const express = require('express')
 
 const router = express.Router()
 
+const contactsOperations = require("../../models/contacts")
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await contactsOperations.listContacts()
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result: contacts
+      }
+  })
+    
+  } catch (error) {
+    res.status(500).json({
+      status:"error",
+      code: 500,
+      massege: "Server error",
+    })
+    
+  }
+  
 })
 
 router.get('/:contactId', async (req, res, next) => {
